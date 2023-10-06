cb6808a37cd434b7688d60567f36dfee4f238d76
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-06 17:33:15 -0400

    (Refactor): Imports Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Header } from '@/components/header'
 import cn from 'classnames'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={cn(inter.className, 'relative bg-gray-50 text-gray-950')}>
+      <body className={cn(inter.className, 'relative bg-gray-50')}>
+        <div className='-z-10 bg-blue-200 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] rounded-full blur-[10rem]' />
+        <div className='-z-10 bg-green-100 absolute top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] rounded-full blur-[10rem]' />
+        <Header />
         {children}
       </body>
     </html>
