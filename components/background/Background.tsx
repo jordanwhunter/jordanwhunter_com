3acf117ca22cb5e795460a20b778d1f87254fba7
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-07 12:01:55 -0400

    (Style): Styles Background component

---
+++ components/background/Background.tsx
@@ -0,0 +1,8 @@
+const Background = () => (
+  <>
+    <div className="-z-10 bg-blue-200 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] rounded-full blur-[10rem]" />
+    <div className="-z-10 bg-green-100 absolute top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] rounded-full blur-[10rem]" />
+  </>
+);
+
+export default Background
