9a6207dfa0fc4bbbcbe6a99dc587b741746a3fac
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-07 12:04:49 -0400

    (Refactor): Imports SITE_NAME, SITE_DESCRIPTION variables and Background component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,16 @@ import './globals.css'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SITE_NAME, SITE_DESCRIPTION } from '@/lib/constants'
+import { Background } from '@/components/background'
 import { Header } from '@/components/header'
 import cn from 'classnames'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Jordan W. Hunter: React | Next.js Developer',
-  description: 'Jordan Hunter (he/him) is an avid full-stack JavaScript developer residing in New York City. This portfolio page highlights some of his most recent projects/developments - both agency-assigned and freelance.',
+  title: SITE_NAME,
+  description: SITE_DESCRIPTION,
 };
 
 export default function RootLayout({
@@ -20,8 +22,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn(inter.className, 'relative bg-gray-50')}>
-        <div className='-z-10 bg-blue-200 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] rounded-full blur-[10rem]' />
-        <div className='-z-10 bg-green-100 absolute top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[31.25rem] sm:w-[68.75rem] rounded-full blur-[10rem]' />
+        <Background />
         <Header />
         {children}
       </body>
