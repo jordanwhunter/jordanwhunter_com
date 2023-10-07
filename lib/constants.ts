901ed7f8a4f04cc723490cd8db81e9b4ad89fc02
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-07 12:00:59 -0400

    (Config): Defines SITE_NAME and SITE_DESCRIPTION

---
+++ lib/constants.ts
@@ -0,0 +1,34 @@
+// Brand
+export const SITE_NAME = 'Jordan W. Hunter: React | Next.js Developer';
+export const SITE_DESCRIPTION =
+  'Jordan Hunter (he/him) is an avid full-stack JavaScript developer residing in New York City. This portfolio page highlights some of his most recent projects/developments - both agency-assigned and freelance.';
+
+// Menu and Navigation
+export const MENU = {
+  navigation: [
+    {
+      name: "Home",
+      hash: "#home",
+    },
+    {
+      name: "About",
+      hash: "#about",
+    },
+    {
+      name: "Projects",
+      hash: "#projects",
+    },
+    {
+      name: "Skills",
+      hash: "#skills",
+    },
+    {
+      name: "Experience",
+      hash: "#experience",
+    },
+    {
+      name: "Contact",
+      hash: "#contact",
+    },
+  ],
+};
