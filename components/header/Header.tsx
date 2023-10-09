8792c2241a578749d0eb0ecb4d7821b48bf717e1
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-09 17:14:20 -0400

    (Refactor): Imports Mobile component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,6 +3,7 @@ import { Desktop, Mobile } from "@/components/header";
 const Header = () => {
   return (
     <header className="relative z-10">
+      <Mobile />
       <Desktop />
     </header>
   );
