0f92a2ec5f5a56820215d71e0e9824cf21af6363
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-09 17:15:03 -0400

    (Style): Styles DesktopHeader nav bar

--- components/header/Desktop.tsx
+++ components/header/Desktop.tsx
@@ -1,14 +1,36 @@
 'use client'
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
+import { MENU } from '@/lib/constants'
+import Link from 'next/link'
 
 const DesktopHeader = () => {
   return (
     // dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75
-    <motion.div
-      className="max-md:hidden fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
-      initial={{ y: -100, x: "-50%", opacity: 0 }}
-      animate={{ y: 0, x: "-50%", opacity: 1 }}
-    />
+    <>
+      <motion.div
+        className="max-md:hidden fixed top-6 left-1/2 -translate-x-1/2 rounded-full border border-white border-opacity-40 bg-white bg-opacity-40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] h-[3.25rem] w-[36rem]"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      >
+        <nav className="flex fixed left-1/2 -translate-x-1/2">
+          <ul className="flex w-[22rem] items-center justify-center gap-5 text-[0.9rem] font-medium text-gray-400">
+            {MENU.navigation.map((link) => (
+              <li
+                key={link.href}
+                className="h-3/4 flex items-center justify-center"
+              >
+                <Link
+                  href={link.href}
+                  className="hover:text-gray-600 flex w-full items-center justify-center p-3 transition"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </motion.div>
+    </>
   );
 };
 
