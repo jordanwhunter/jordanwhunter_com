1963caca202e1dc0414cf021be4391eb493e9665
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-12 17:55:58 -0400

    (Style): Adds more top padding on smaller breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from '@/components/hero'
 
 export default function Home() {
   return (
-    <main className='flex flex-col container mx-auto px-4 pb-4 pt-8 lg:pt-16'>
+    <main className='flex flex-col container mx-auto px-4 pb-4 pt-8 md:pt-28'>
       <Hero />
     </main>
   )
