055f159144a5738178e063302274e8bd65b12536
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-12 17:58:28 -0400

    (Refactor): Tweaks ArrowProps and adds new ButtonProps interface

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,14 @@
 // @/components/ui/Arrow.tsx
 export interface ArrowProps {
-  caption: string;
-  size: string;
-  direction: string;
+  text?: string;
+  size?: string;
+  direction?: string;
+  arrow?: boolean;
+  download?: boolean;
 }
 
 export interface SizeProps {
+  '2xl': string;
   xl: string;
   lg: string;
   default: string;
@@ -17,4 +20,18 @@ export interface DirectionProps {
   default: string;
   down: string;
   right: string;
+}
+
+// @/components/ui/Button.tsx
+export interface ButtonProps {
+  href?: string;
+  text?: string;
+  size?: string;
+  direction?: string;
+  arrow?: boolean;
+  download?: boolean;
+  internal?: boolean;
+  linkedin?: boolean;
+  github?: boolean;
+  classProps?: string;
 }
