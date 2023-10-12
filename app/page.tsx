c7db868ca32c3979486bd1b3bb73242dc4ff4aaa
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-11 20:57:48 -0400

    (Style): Adds padding for larger viewports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { Hero } from "@/components/hero"
+import { Hero } from '@/components/hero'
 
 export default function Home() {
   return (
-    <main className='flex flex-col container mx-auto px-4 pb-4 pt-8'>
+    <main className='flex flex-col container mx-auto px-4 pb-4 pt-8 lg:pt-16'>
       <Hero />
     </main>
   )
