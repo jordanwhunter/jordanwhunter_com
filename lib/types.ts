179d871a79bdae1af36a2a541fff31e0c2a77cf6
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-11 21:02:44 -0400

    (Config): Adds types for LinkArrow component

---
+++ lib/types.ts
@@ -0,0 +1,20 @@
+// @/components/ui/Arrow.tsx
+export interface ArrowProps {
+  caption: string;
+  size: string;
+  direction: string;
+}
+
+export interface SizeProps {
+  xl: string;
+  lg: string;
+  default: string;
+  sm: string;
+  xs: string;
+}
+
+export interface DirectionProps {
+  default: string;
+  down: string;
+  right: string;
+}
