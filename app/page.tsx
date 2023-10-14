4b54a60e58a624e8c8141537161afbecec07c8e7
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-14 19:52:55 -0400

    (Refactor): Imports SectionDivider and About components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Hero } from '@/components/hero'
+import { SectionDivider, About } from '@/components/content'
 
 export default function Home() {
   return (
-    <main className='flex flex-col container mx-auto px-4 pb-4 pt-8 md:pt-28'>
+    <main className="flex flex-col items-center container mx-auto px-4 pb-4 pt-8 md:pt-28">
       <Hero />
+      <SectionDivider />
+      <About />
     </main>
-  )
+  );
 }
