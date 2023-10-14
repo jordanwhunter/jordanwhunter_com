c1526a78e7654533326ec4859d3816a537781efe
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-14 19:49:12 -0400

    (Config): Exports defaults

---
+++ components/content/index.ts
@@ -0,0 +1,4 @@
+export { default as SectionDivider } from './Divider'
+export { default as SectionHeading } from './Heading'
+export { default as About } from './About'
+export { default as Bio } from './Bio'
