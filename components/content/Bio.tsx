4b113266deae8e28939f90040467220619e87d7a
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-10-20 15:14:13 -0400

    (Style): Increases padding on text block

--- components/content/Bio.tsx
+++ components/content/Bio.tsx
@@ -1,5 +1,5 @@
 const Bio = () => (
-  <p className='pb-3 max-sm:px-2 text-gray-400 leading-8 text-center lg:text-left'>
+  <p className='pb-3 max-sm:px-4 text-gray-400 leading-8 text-center lg:text-left'>
     A React | Next.js programmer residing in New York City with multiple years of professional work experience. Graduated from the {' '} <span className='font-bold'>University of South Florida</span> in 2016 with a STEM bachelor&apos;s degree, and in 2020 from {' '} <span className='font-bold'>Columbia University</span>&apos;s Full-Stack JavaScript Development bootcamp. A passionate, goal-oriented engineer that strives to always write clean, precise code with a {' '} <span className='italic'>mobile-first</span> approach. Maintains a problem-solving, can-do attitude and exhibits consistent eagerness to learn new technologies/techniques. 
   </p>
 )
