5c749705b43360ffe2cffc69b2dccc045d1d7784
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-11-28 17:06:23 -0500

    (Refactor): Sets up mobile parameter for breakpoint styling

--- components/content/Divider.tsx
+++ components/content/Divider.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { motion } from 'framer-motion'
+import cn from 'classnames'
 
-const Divider = () => (
+const Divider = ({ mobile = false }) => (
   <motion.div
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ delay: 0.125 }}
-    className='bg-gray-200 my-6 h-24 w-1 rounded-full'
+    className={cn('bg-gray-200 my-6 h-24 w-1 rounded-full', mobile && 'lg:hidden')}
   />
 );
 
