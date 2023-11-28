f5054ca38493d4c0a8cacc71e26ec2593e65d3e4
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-11-28 17:07:50 -0500

    (Feature): Sets ref for Contact component

---
+++ components/content/Contact.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { SectionHeading } from '@/components/content'
+import { useSectionInView } from '@/lib/hooks'
+
+const Contact = () => {
+  const { ref } = useSectionInView({
+    sectionName: 'Contact',
+    threshold: 0.5,
+  })
+
+  return (
+    <section
+      id='contact'
+      ref={ref}
+      className='scroll-mt-16 sm:scroll-mt-24 lg:scroll-mt-28'
+    >
+      <SectionHeading classProps='mt-6'>Contact</SectionHeading>
+    </section>
+  )
+}
+
+export default Contact
