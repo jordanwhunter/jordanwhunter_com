c5de24a2f617f5c6244a23e9c0827b14d79910b4
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-11-30 10:16:31 -0500

    (Refactor): Imports Toaster and Footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import { Inter } from 'next/font/google'
 import { SITE_NAME, SITE_DESCRIPTION } from '@/lib/constants'
 import { Background } from '@/components/background'
 import { Header } from '@/components/header'
+import { Footer } from '@/components/footer'
 import { ActiveContextProvider } from '@/context'
+import { Toaster } from 'react-hot-toast'
 import cn from 'classnames'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,14 +23,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="!scroll-smooth">
-      <body className={cn(inter.className, "relative bg-gray-50")}>
+    <html lang='en' className='!scroll-smooth'>
+      <body className={cn(inter.className, 'relative bg-gray-50')}>
         <Background />
         <ActiveContextProvider>
           <Header />
           {children}
+          <Toaster position='bottom-center' />
+          <Footer />
         </ActiveContextProvider>
       </body>
     </html>
-  );
+  )
 }
