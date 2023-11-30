49d859833234e88a14811bf7a31dcd6120349e15
jordanwhunter <jordanwhunter.mail@gmail.com>
2023-11-30 10:15:01 -0500

    (Feature): Handles email string validation

---
+++ lib/utils/validateString.ts
@@ -0,0 +1,9 @@
+const validateString = (value: unknown, maxLength: number) => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
+
+  return true;
+};
+
+export default validateString
